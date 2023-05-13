6635a05e142ed0a42b9d69c44a90e45c38d7a5b8
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-05-13 11:05:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Counter</h1>
+        <div className="counter">
+          <button onClick={() => count <= -10 ? "" : setCount (count - 1)}>-</button>
+          <h1>{count}</h1>
+          <button onClick={() => count >= 10 ? "" : setCount (count + 1)}>+</button>
+        </div>
+      </div>
     </div>
   );
 }
